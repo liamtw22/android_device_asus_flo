c0e6b25a1c1fbb5b6492c6ff1c6675b4a842fb84
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2021-01-31 23:02:51 -0500

    Update havoc_flo.mk

--- havoc_flo.mk
+++ havoc_flo.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, device/asus/flo/device.mk)
 
 # Shipping API
-$(call inherit-product, vendor/lineage/build/target/product/product_launched_with_j_mr2.mk)
+$(call inherit-product, vendor/havoc/build/target/product/product_launched_with_j_mr2.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := flo
